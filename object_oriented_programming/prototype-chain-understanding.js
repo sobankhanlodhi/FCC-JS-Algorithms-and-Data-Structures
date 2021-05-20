9487c124fccecb52781ff8ed9662859bc1768823
Soban Khan Lodhi <sobankhanlodhi@gmail.com>
2021-05-20 23:51:34 +0500

    Understand the Prototype Chain

--- object_oriented_programming/prototype-chain-understanding.js
+++ object_oriented_programming/prototype-chain-understanding.js
@@ -37,10 +37,6 @@ Modify the code to show the correct prototype chain.
 function Dog(name) {
     this.name = name;
 }
-
 let beagle = new Dog("Snoopy");
-
-Dog.prototype.isPrototypeOf(beagle);  // yields true
-
-// Fix the code below so that it evaluates to true
+Dog.prototype.isPrototypeOf(beagle);
 Object.prototype.isPrototypeOf(Dog.prototype);
